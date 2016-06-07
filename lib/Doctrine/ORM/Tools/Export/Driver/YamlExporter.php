2626af3d8998173f71813f0551ad7514b8924482
Thomas Ploch <thomas.ploch@meinfernbus.de>
2016-06-07 17:34:23 +0200

    Only inspect joinColumns for owning side in bi-directional OneToOne in YamlExporter

--- lib/Doctrine/ORM/Tools/Export/Driver/YamlExporter.php
+++ lib/Doctrine/ORM/Tools/Export/Driver/YamlExporter.php
@@ -163,7 +163,7 @@ class YamlExporter extends AbstractExporter
             }
 
             if ($associationMapping['type'] & ClassMetadataInfo::TO_ONE) {
-                $joinColumns = $associationMapping['joinColumns'];
+                $joinColumns = $associationMapping['isOwningSide'] === true ? $associationMapping['joinColumns'] : array();
                 $newJoinColumns = array();
 
                 foreach ($joinColumns as $joinColumn) {
